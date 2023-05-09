d18966d1d7e5b1d7c25ebedf17d084ea32b072b3
Alfredo20164920 <apaz20164920@gmail.com>
2023-05-09 12:23:52 -0600

    feat: styles

--- pages/register.js
+++ pages/register.js
@@ -4,7 +4,72 @@ import React from 'react'
 const Register = () => {
     return (
         <Layout>
-            <h2 className="text-2xl text-white font-light">Register</h2>
+            <h1 className="text-2xl text-white text-center font-light">Register</h1>
+
+            <div className='flex justify-center mt-5'>
+                <div className='w-full max-w-sm'>
+                    <form
+                        className='bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4 flex flex-col gap-5'
+                    >
+                        <div>
+                            <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='name'>
+                                Name
+                            </label>
+
+                            <input 
+                                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow'
+                                id='name'
+                                type='text'
+                                placeholder='John'
+                            />
+                        </div>
+
+                        <div>
+                            <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='lastName'>
+                                Last Name
+                            </label>
+                            <input 
+                                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow'
+                                id='lastname'
+                                type='text'
+                                placeholder='Doe'
+                            />
+                        </div>
+
+                        <div>
+                            <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='email'>
+                                Email
+                            </label>
+                            <input 
+                                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow'
+                                id='email'
+                                type='email'
+                                placeholder='example@example.com'
+                            />
+                        </div>
+
+                        <div>
+                            <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='password'>
+                                Password
+                            </label>
+                            <input 
+                                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-inner'
+                                id='password'
+                                type='password'
+                                placeholder='.........'
+                            />
+                        </div>
+
+                        <input 
+                            type='submit'
+                            className='bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-700'
+                            value="Login"
+                        />
+
+                    </form>
+                </div>
+            </div>
+
         </Layout>
     )
 }
