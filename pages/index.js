e529aede98201dd25ca7ecf1acb425cd95413a7a
Alfredo Paz <alfredo.paz@ironbit.com.mx>
2023-05-09 10:35:00 -0600

    Fix: delete fonts from next

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,4 @@
-import Image from "next/image";
-import { Lexend } from "next/font/google";
 import Layout from "@/components/Layout";
-
-const Lexend = Lexend({ subsets: ["latin"] });
 
 export default function Home() {
 	return (
