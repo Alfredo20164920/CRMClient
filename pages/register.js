d43a0205dfa5cb070e9f022f73f6b82f9fcd9694
Alfredo20164920 <apaz20164920@gmail.com>
2023-05-09 14:15:17 -0600

    feat: add functionality to register

--- pages/register.js
+++ pages/register.js
@@ -1,32 +1,91 @@
 import Layout from '@/components/Layout'
+import { useMutation, gql } from '@apollo/client'
 import { useFormik } from 'formik'
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useState } from 'react'
 import * as Yup from 'yup'
 
+const CREATE_CLIENT = gql`
+    mutation NewUser($input: UserInput) {
+        newUser(input: $input) {
+            id,
+            name,
+            lastName,
+            email
+        }
+    }
+`;
+
 const Register = () => {
+    const [message, setMessage] = useState(null)
+
+    // Mutation to create new user
+    const [ newUser ] = useMutation(CREATE_CLIENT);
+
+    // Routing
+    const router = useRouter();
 
     // VALIDATE FORM
     const formik = useFormik({
         initialValues: {
             name: '',
-            lastname: '',
+            lastName: '',
             email: '',
             password: '',
         },
         validationSchema: Yup.object({
             name: Yup.string().required('Name is required'),
-            lastname: Yup.string().required('Lastname is requiered'),
+            lastName: Yup.string().required('Lastname is requiered'),
             email: Yup.string().email('Invalid email').required('Email is required'),
             password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required')
         }),
-        onSubmit: values => {
-            console.log("Sending");
-            console.log(values);
+        onSubmit: async values => {
+
+            const { name, lastName, email, password } = values;
+
+            try {
+                const { data } = await newUser({
+                    variables: {
+                        input: {
+                            name,
+                            lastName,
+                            email,
+                            password
+                        }
+                    }
+                });
+
+                console.log(data)
+                setMessage(`It was create succesfuly user: ${data.newUser.name}, wait a second`)
+                
+                setTimeout( () => {
+                    setMessage(null);
+                    router.push('/login');
+                }, 2000)
+
+            } catch (error) {
+                setMessage(error.message);
+
+                setTimeout(() => {
+                    setMessage(null)
+                }, 3000);
+
+            }
         }
     });
 
+    const showMessage = () => {
+        return (
+            <div className='bg-green-500 py-2 px-3 w-full my-3 max-w-sm text-center mx-auto rounded'>
+                <p className='text-white'>{message}</p>
+            </div>
+        )
+    }
+
     return (
         <Layout>
+            {message && showMessage()}
+
             <h1 className="text-2xl text-white text-center font-light">Register</h1>
 
             <div className='flex justify-center mt-5'>
@@ -62,26 +121,26 @@ const Register = () => {
                         }
 
                         <div>
-                            <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='lastname'>
+                            <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='lastName'>
                                 Last Name
                             </label>
                             <input 
                                 className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow'
-                                id='lastname'
+                                id='lastName'
                                 type='text'
                                 placeholder='Doe'
-                                value={formik.values.lastname}
+                                value={formik.values.lastName}
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
                             />
                         </div>
 
                         {
-                            formik.touched.lastname && formik.errors.lastname ? 
+                            formik.touched.lastName && formik.errors.lastName ? 
                             (
                                 <div className='my-2 bg-red-100 border-l-4 border-red-500 text-red-500 p-4'>
                                     <p className='font-bold'>Error</p>
-                                    <p>{formik.errors.lastname}</p>
+                                    <p>{formik.errors.lastName}</p>
                                 </div>
                             ) : null
                         }
